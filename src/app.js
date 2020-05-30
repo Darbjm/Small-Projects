daf543e08b9ef46c46a93e8f7c3f39db20a0d806
James Darby <jamesdarby1@hotmail.com>
2020-05-30 10:47:22 +0100

    conditional render

--- src/app.js
+++ src/app.js
@@ -10,13 +10,48 @@ import Rotate from './rotate/Rotate'
 
 
 class App extends Component {
+  state = {
+    width: 0,
+    height: 0
+  }
+
+  componentDidMount() {
+    window.scrollTo(0, 0)
+    this.updateWindowDimensions()
+    window.addEventListener('resize', this.updateWindowDimensions)
+  }
+
+  constructor(props) {
+    super(props)
+    this.state = { width: 0, height: 0 }
+    this.updateWindowDimensions = this.updateWindowDimensions.bind(this)
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.updateWindowDimensions)
+  }
+
+  updateWindowDimensions() {
+    this.setState({ width: window.innerWidth, height: window.innerHeight })
+  }
+
+  renderChart = () => {
+    const { width } = this.state
+    if (width === 0) {
+      return 'No data yet'
+    }
+    if (width % 2 === 0) {
+      return <div><Route path="/rotate" component={Rotate} /></div>
+    }
+    return <Route path="/rotate" component={Rotate} />
+  }
   render() {
     return (
       <BrowserRouter>
         <Switch>
           <Route exact path="/" component={Yandex} />
           <Route path="/d3" component={D3} />
-          <Route path="/rotate" component={Rotate} />
+          {this.renderChart()}
         </Switch>
       </BrowserRouter>
     )
